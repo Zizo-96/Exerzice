7f6eff9946b761f24bf77376527482af1a345bc1
Abdelaziz Mohamed <147454357+Zizo-96@users.noreply.github.com>
2024-05-18 22:00:29 +0000

    Added welcome message with userName on Profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,53 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
+import { jwtDecode } from 'jwt-decode';
 
 function Profile() {
+    let token;
+    let decoded;
+    if (localStorage.getItem('token')) {
+        token = localStorage.getItem('token');
+        decoded = jwtDecode(token);
+    }
+
     return (
-        <Box>
-            Hello
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+                padding: 3
+            }}
+        >
+            <Paper
+                elevation={3}
+                sx={{
+                    padding: 4,
+                    borderRadius: 2,
+                    backgroundColor: '#ffffff',
+                    maxWidth: 600,
+                    textAlign: 'center',
+                }}
+            >
+                <Typography
+                    variant='h4'
+                    component='h1'
+                    sx={{
+                        fontWeight: 'bold',
+                        color: 'primary.main'
+                    }}
+                    gutterBottom
+                >
+                    Welcome back, {decoded ? decoded.username : 'Guest'}!
+                </Typography>
+                <Typography
+                    variant='body1'
+                    sx={{
+                        color: '#grey'
+                    }}
+                >
+                    Unleash Your Inner Warrior.
+                </Typography>
+            </Paper>
         </Box>
     );
 }
